c02e24f8669f03c610dfb09ace1fdc4a16b55468
Administrator <Administrator@10.252.109.164>
2022-10-19 17:30:15 +0530

    DBMS with login

---
+++ jdbctoLogInUp/src/jdbctoLogInUp/register.java
@@ -0,0 +1,13 @@
+package jdbctoLogInUp;
+import java.sql.*;
+public class register {
+	public register(String name,String passW)throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost/Userdata","root","0000");
+		PreparedStatement st=con.prepareStatement("insert into accountdetail value('"+name+"','"+passW+"')");
+		st.executeUpdate();
+		System.out.println("Register successfully");
+	    st.close();
+	    con.close();
+	}
+}
